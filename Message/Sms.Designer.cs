ddad701cd4a540d25af9f20695c1ad11dd8f9fbe
KayzenCJS <carolayncjs@gmail.com>
2022-10-03 22:47:36 -0300

    Finalizado

---
+++ Message/Sms.Designer.cs
@@ -0,0 +1,97 @@
+﻿
+namespace Message
+{
+    partial class Sms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMessage = new System.Windows.Forms.RichTextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtMessage.Location = new System.Drawing.Point(57, 35);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(589, 338);
+            this.txtMessage.TabIndex = 7;
+            this.txtMessage.Text = "";
+            // 
+            // txtTo
+            // 
+            this.txtTo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTo.Location = new System.Drawing.Point(57, 9);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(432, 20);
+            this.txtTo.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "To:";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(482, 392);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(164, 41);
+            this.btnSend.TabIndex = 4;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Sms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 445);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Sms";
+            this.Text = "Sms";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox txtMessage;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
---
+++ Message/Sms.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Message
+{
+    public partial class Sms : Form
+    {
+        public Sms()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                WebClient client = new WebClient();
+                Stream s = client.OpenRead(string.Format("https://platform.clickatell.com/messages/http/send?apiKey=CxvF3WkhRn2V3xH4qHh5aw==&to={0}&content={1}", txtTo.Text, txtMessage.Text));//Este link lo genera una pagina llamaca ClickTell, la cual contiene una key que deberas colocar aca y personalizarla, los espcaciones de memoria son para recibir especificamente ese texto.
+
+                StreamReader reader = new StreamReader(s);
+                string result = reader.ReadToEnd();
+                MessageBox.Show(result, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
